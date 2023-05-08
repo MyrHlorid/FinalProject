d695adf0105d6a996a8dd2defa3a01515679aa7e
Nananina2004 <nurayna.medresova@bk.ru>
2023-05-08 21:56:15 +0600

    added some necessary things

--- job_portal/models.py
+++ job_portal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Candidates (models.Model):
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    resume = models.FileField(null=True)
+    mobile_numb = models.CharField(max_length=200, null=True)
+    dob = models.IntegerField(max_length=150, null=True)
+
+
+
+
+
