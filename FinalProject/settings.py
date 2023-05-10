2ea1964cb8b7a93bb10c2c19470dbc6895df0656
Nananina2004 <nurayna.medresova@bk.ru>
2023-05-10 11:58:55 +0600

    added some necessary things

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'job_add',
+    'crispy_bootstrap4',
     'job_portal',
+    'crispy_forms',
+
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -13,16 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('job_add.urls'))
+    path('', include('job_portal.urls'))
 ]
 
 if settings.DEBUG:
