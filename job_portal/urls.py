e67a1cb3c9995d5668da1ebe3faf1d8611e3fb58
Nananina2004 <nurayna.medresova@bk.ru>
2023-05-10 14:03:47 +0600

    login and registration

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('register/', views.register_request, name="register")
+    path('register/', views.register_request, name="register"),
+    path('login/', views.login_request, name="login")
 ]
--- job_portal/views.py
+++ job_portal/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
-# Create your views here.
 
 def index(request):
-    render(request, "base.html")
+    return render(request, "base.html")
 
 
 def register_request(request):
@@ -20,3 +20,22 @@ def register_request(request):
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request=request, template_name="job_portal/register.html", context={"register_form": form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return render(request, "base.html")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="job_portal/login.html", context={"login_form": form})
