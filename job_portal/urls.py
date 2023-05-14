099de99f479558ec4736e8d0b1338f8525c0065d
Nananina2004 <nurayna.medresova@bk.ru>
2023-05-14 11:39:08 +0600

    job_list

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import profile_view, profile_create, profile_update
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -14,8 +13,10 @@ urlpatterns = [
     path('job_add/', views.create_job, name='add_job_to_user'),
     path('clear_user_jobs/', views.clear_user_jobs, name='clear_user_jobs'),
     path('profile/', views.profile_view, name="profile"),
-    path('profile/<int:pk>/', profile_view, name='profile_view'),
-    path('profile/create/', profile_create, name='profile_create'),
-    path('profile/<int:pk>/update/', profile_update, name='profile_update'),
+    path('profile/<int:pk>/', views.profile_view, name='profile_view'),
+    path('profile/create/', views.profile_create, name='profile_create'),
+    path('profile/<int:pk>/update/', views.profile_update, name='profile_update'),
     path('add_job_to_user/<int:job_id>/', views.add_job_to_user, name='add_job_to_user'),
+    path('search/', views.search_job, name='search_job')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
