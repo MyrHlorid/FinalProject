fdfee4ccb55e617f33032b0826bd36e288566b22
nnnursaule <120462317+nnnursaule@users.noreply.github.com>
2023-05-14 17:48:01 +0600

    Update admin.py

--- job_portal/admin.py
+++ job_portal/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import *
-from .models import Profile
+
 
 admin.site.register(Profile)
 admin.site.register(Candidates)
 admin.site.register(Job)
+admin.site.register(User)
+
